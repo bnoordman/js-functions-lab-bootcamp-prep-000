948623b8bb9132d10df9068f1ea14b2b84c1dc31
Brittany Young <mrsbrityoung@gmail.com>
2018-01-21 04:26:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(${name}) {
+function happyHolidaysTo($name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(${holiday}, ${name}) {
+function happyHolidaysTo($holiday, $name) {
   return `Happy ${holiday}, ${name}!`
 }
 
